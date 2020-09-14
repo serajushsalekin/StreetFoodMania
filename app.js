6767d222dc7ca4190c415a7de59a6262517d6ad9
salekin <serajushsalekin@gmail.com>
2020-09-14 15:21:47 +0600

    [ADD] Stall Model

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const env = require('dotenv')
+const mongoose = require('mongoose')
+
+
+// router imports
+const usersRouter = require('./routes/users');
+
+
+// env config
+env.config()
+
+// db
+const url = "mongodb://localhost:27017/food_db"
+const connect = mongoose.connect(url, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, useCreateIndex: true})
+connect.then(db => console.log("Connected to db successfully")).catch(err=> console.log(err))
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routers
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ models/Stall.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const stallSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    photos: [
+        {
+            img: {
+                type: String
+            }
+        }
+    ],
+    reviews : [
+        {
+            userId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            rating: {
+                type: Number,
+                min: 1,
+                max: 5,
+                required: true
+            },
+            review: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type:[Number],
+            index: '2dsphere',
+            required: true
+        }
+    }
+}, { timestamps: true})
+
+module.exports = mongoose.model('Stall', stallSchema)
