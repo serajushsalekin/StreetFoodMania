ba271222c1c94d45c1b4f82660e81a972b3a4040
salekin <serajushsalekin@gmail.com>
2020-10-03 23:43:38 +0600

    [Fix] whitelist

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://127.0.0.1:5000'
+const url = 'http://streetfoodmania.herokuapp.com'
 
 const api = axios.create({
     baseURL: url
--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:5000', 'http://localhost:3000', 'http://127.0.0.1:3000'];
+const whitelist = ['http://localhost:5000',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://streetfoodmania.herokuapp.com',
+    'https://streetfoodmania.herokuapp.com'
+];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
